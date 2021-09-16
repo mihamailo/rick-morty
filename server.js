0d982efc13890690468a2689fe9edb40b5699676
Mikhail <zemiza@yandex.ru>
2021-09-16 10:39:06 +0300

    make it better

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 const express = require('express');
-const path = require('path');
-
+// const path = require('path');
+const port = process.env.PORT || 8080;
 const app = express();
 
-app.use(express.static(__dirname))
-app.use(express.static(path.resolve(__dirname, 'dist')))
+app.use(express.static('build'))
+app.listen(port, () => console.log(`on port ${port}`))
+// app.use(express.static(__dirname))
+// app.use(express.static(path.resolve(__dirname, 'dist')))
 
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist', 'index.html'))
-})
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'dist', 'index.html'))
+// })
 
-const port = process.env.PORT || 8080;
-app.listen(port)
+// app.listen(port)
 
-console.log('current port: ' + port);
+// console.log('current port: ' + port);
